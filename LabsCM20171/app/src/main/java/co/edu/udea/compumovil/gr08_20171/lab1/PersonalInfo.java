94f1213e21d0f35a34cebf9f248aa69596522d45
nilton velez <nilton velez>
2017-02-19 15:01:26 -0500

    actividad1 actualizada

--- LabsCM20171/app/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/PersonalInfo.java
+++ LabsCM20171/app/src/main/java/co/edu/udea/compumovil/gr08_20171/lab1/PersonalInfo.java
@@ -1,7 +1,12 @@
 package co.edu.udea.compumovil.gr08_20171.lab1;
 
+import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PersonalInfo extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class PersonalInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_info);
+       // arraySpinner();
     }
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+    public void arraySpinner(){
+        Spinner spinner = (Spinner) findViewById(R.id.options_scholarship);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        R.array.grados, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
 }
